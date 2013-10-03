5f0598fe156ef9f0251f7e39459fd4f12221cefc
Chyld Medford <chyld.medford@gmail.com>
2013-10-03 11:13:54 -0500

    photoshop lite 2

--- 2013-10-02-Photoshop-Lite/js/main.js
+++ 2013-10-02-Photoshop-Lite/js/main.js
@@ -1,7 +1,34 @@
+'use strict';
+
 $(document).ready(initialize);
 
 function initialize(){
   $('#add_color').click(addColor);
+  $('#add_box').click(addBox);
+  $('#colors').on('click', '.box', colorPaletteClicked);
+  $('#boxes').on('mouseover', '.canvas', canvasHover);
+}
+
+function canvasHover(){
+  var $canvas = $(this);
+  var brushColor = $('#brush').css('background-color');
+  $canvas.css('background-color', brushColor);
+}
+
+function colorPaletteClicked(){
+  var $box = $(this);
+  var color = $box.css('background-color');
+  $('#brush').css('background-color', color);
+}
+
+function addBox(){
+  var amount = $('#amount').val();
+  amount = parseInt(amount, 10);
+  for(var i = 0; i < amount; i++){
+    var $canvas = $('<div>');
+    $canvas.addClass('canvas');
+    $('#boxes').append($canvas);
+  }
 }
 
 function addColor(){
