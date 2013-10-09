3ebb217cb302ef89733c0deccfd5a6ed78f40821
Chyld Medford <chyld.medford@gmail.com>
2013-10-09 12:11:10 -0500

    inventory

--- 2013-10-08-Inventory-Firebase/js/app/app.js
+++ 2013-10-08-Inventory-Firebase/js/app/app.js
@@ -1,6 +1,7 @@
 'use strict';
 
-var db;
+var Δdb;
+var Δitems;
 var items;
 
 $(document).ready(initialize);
@@ -9,14 +10,28 @@ function initialize(){
   $(document).foundation();
   $('#add').click(add);
   $('#save').click(save);
-  db = new Firebase('https://inventory-cm.firebaseio.com/');
-  items = db.child('items');
 
-  db.on('value', function(snapshot) {
-    var inventory = snapshot.val();
-    $('#person').val(inventory.fullName);
-    $('#address').val(inventory.address);
-  });
+  Δdb = new Firebase('https://inventory-cm.firebaseio.com/');
+  Δitems = Δdb.child('items');
+  Δdb.on('value', receiveDb);
+}
+
+function receiveDb(snapshot){
+  var inventory = snapshot.val();
+  $('#person').val(inventory.fullName);
+  $('#address').val(inventory.address);
+
+  if(inventory.items){
+    items = inventory.items;
+  } else {
+    items = [];
+  }
+
+  var $header = $('#items tr:first-child').detach();
+  $('#items').empty().append($header);
+  for(var i = 0; i < items.length; i++){
+    createRow(items[i]);
+  }
 }
 
 function save(){
@@ -26,7 +41,7 @@ function save(){
   inventory.fullName = fullName;
   inventory.address = address;
 
-  db.set(inventory);
+  Δdb.update(inventory);
 }
 
 function add(){
@@ -37,16 +52,6 @@ function add(){
   var condition = $('#condition').val();
   var date = $('#date').val();
 
-  var row = '<tr><td class="name"></td><td class="count"></td><td class="value"></td><td class="room"></td><td class="condition"></td><td class="date"></td></tr>';
-  var $row = $(row);
-
-  $row.children('.name').text(name);
-  $row.children('.count').text(count);
-  $row.children('.value').text(value);
-  $row.children('.room').text(room);
-  $row.children('.condition').text(condition);
-  $row.children('.date').text(date);
-
   var item = {};
   item.name = name;
   item.count = count;
@@ -56,6 +61,19 @@ function add(){
   item.date = date;
 
   items.push(item);
+  Δitems.set(items);
+}
+
+function createRow(item){
+  var row = '<tr><td class="name"></td><td class="count"></td><td class="value"></td><td class="room"></td><td class="condition"></td><td class="date"></td></tr>';
+  var $row = $(row);
+
+  $row.children('.name').text(item.name);
+  $row.children('.count').text(item.count);
+  $row.children('.value').text(item.value);
+  $row.children('.room').text(item.room);
+  $row.children('.condition').text(item.condition);
+  $row.children('.date').text(item.date);
 
   $('#items').append($row);
 }
