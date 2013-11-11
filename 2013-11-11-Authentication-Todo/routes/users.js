f069646bb9892b96668a7984a20b8e1183337be1
Chyld Medford <chyld.medford@gmail.com>
2013-11-11 14:22:08 -0600

    auth2

--- 2013-11-11-Authentication-Todo/routes/users.js
+++ 2013-11-11-Authentication-Todo/routes/users.js
@@ -1,3 +1,18 @@
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
+var User = mongoose.model('User');
+
 exports.create = function(req, res){
-  res.send('hey i got this!!!');
+  var user = new User();
+  user.email = req.body.email;
+
+  bcrypt.hash(req.body.password, 10, function(err, hash){
+    user.password = hash;
+    user.save(function(err, user){
+      if(err)
+        res.send({status: 'error'});
+      else
+        res.send({status: 'ok'});
+    });
+  });
 }
